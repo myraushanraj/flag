63dbb74277009a7e9912fc53086d5f1535aea32c
raushan2021 <raushanraj@edge196.com>
2023-07-01 22:52:33 +0530

    taeway added

---
+++ product.js
@@ -0,0 +1,75 @@
+const productList = {
+    craftyGirl:[
+        {
+            name:"Carrot Orange",
+            price:"11",
+            desc:"",
+            image:"1.jpg",
+            gateway:"d7425882-2e3b-46ab-b545-2608fc223b9e"
+        },
+        {
+            name:"Red Tomato",
+            price:"12",
+            desc:"",
+            image:"2.jpg",
+            gateway:"a761e72e-905d-4d70-82c8-fb3af1fee83f"
+        },
+        // {
+        //     name:"Carrot Orange",
+        //     price:"11",
+        //     desc:"",
+        //     image:"3.jpg",
+        //     gateway:"3a21f13d-9e6c-4d7e-87e3-209e58f86542"
+        // },
+        {
+            name:"Pepper-Mint",
+            price:"9",
+            desc:"",
+            image:"1.jpg",
+            gateway:"82098f37-82f3-4a21-bba6-0461182bd30a"
+        },
+        {
+            name:"Lavender",
+            price:"12",
+            desc:"",
+            image:"2.jpg",
+            gateway:"c560a9f2-1899-494c-9271-5da953326f39"
+        },
+        {
+            name:"Apple Mangosteen",
+            price:"11",
+            desc:"",
+            image:"3.jpg",
+            gateway:"04474b75-ae00-4a0d-8aa8-b3ed38c78ece"
+        },
+        {
+            name:"Charcoal",
+            price:"9",
+            desc:"",
+            image:"1.jpg",
+            gateway:"7d3928f3-7525-4152-930c-e9a8d067eae3"
+        },
+        {
+            name:"Pineapple Ginger",
+            price:"11",
+            desc:"",
+            image:"2.jpg",
+            gateway:"046238f8-ec6c-4dde-a732-9230b7dc71a2"
+        },
+        {
+            name:"Blue Pea",
+            price:"11",
+            desc:"",
+            image:"3.jpg",
+            gateway:"031403e1-ef5b-4310-aad2-ebfe85727cb7"
+        },
+        {
+            name:"Golden Latte",
+            price:"13",
+            desc:"",
+            image:"1.jpg",
+            gateway:"fe1334fc-2614-47c4-9237-a63610f8ff83"
+        },
+        
+    ]
+}
