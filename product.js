e92b3a48576eb4a45c5f2f5eacab6f3a9b92971b
raushan2021 <raushanraj@edge196.com>
2023-07-06 17:39:25 +0530

    add payment gateway

--- product.js
+++ product.js
@@ -1,18 +1,18 @@
 const productList = {
-    craftyGirl:[
+    craftyGirl: [
         {
-            name:"Carrot Orange",
-            price:"11",
-            desc:"",
-            image:"1.jpg",
-            gateway:"d7425882-2e3b-46ab-b545-2608fc223b9e"
+            name: "Carrot Orange",
+            price: "11",
+            desc: "",
+            image: "1.jpg",
+            gateway: "d7425882-2e3b-46ab-b545-2608fc223b9e"
         },
         {
-            name:"Red Tomato",
-            price:"12",
-            desc:"",
-            image:"2.jpg",
-            gateway:"a761e72e-905d-4d70-82c8-fb3af1fee83f"
+            name: "Red Tomato",
+            price: "12",
+            desc: "",
+            image: "2.jpg",
+            gateway: "a761e72e-905d-4d70-82c8-fb3af1fee83f"
         },
         // {
         //     name:"Carrot Orange",
@@ -22,54 +22,141 @@ const productList = {
         //     gateway:"3a21f13d-9e6c-4d7e-87e3-209e58f86542"
         // },
         {
-            name:"Pepper-Mint",
-            price:"9",
-            desc:"",
-            image:"1.jpg",
-            gateway:"82098f37-82f3-4a21-bba6-0461182bd30a"
+            name: "Pepper-Mint",
+            price: "9",
+            desc: "",
+            image: "1.jpg",
+            gateway: "82098f37-82f3-4a21-bba6-0461182bd30a"
         },
         {
-            name:"Lavender",
-            price:"12",
-            desc:"",
-            image:"2.jpg",
-            gateway:"c560a9f2-1899-494c-9271-5da953326f39"
+            name: "Lavender",
+            price: "12",
+            desc: "",
+            image: "2.jpg",
+            gateway: "c560a9f2-1899-494c-9271-5da953326f39"
         },
         {
-            name:"Apple Mangosteen",
-            price:"11",
-            desc:"",
-            image:"3.jpg",
-            gateway:"04474b75-ae00-4a0d-8aa8-b3ed38c78ece"
+            name: "Apple Mangosteen",
+            price: "11",
+            desc: "",
+            image: "3.jpg",
+            gateway: "04474b75-ae00-4a0d-8aa8-b3ed38c78ece"
         },
         {
-            name:"Charcoal",
-            price:"9",
-            desc:"",
-            image:"1.jpg",
-            gateway:"7d3928f3-7525-4152-930c-e9a8d067eae3"
+            name: "Charcoal",
+            price: "9",
+            desc: "",
+            image: "1.jpg",
+            gateway: "7d3928f3-7525-4152-930c-e9a8d067eae3"
         },
         {
-            name:"Pineapple Ginger",
-            price:"11",
-            desc:"",
-            image:"2.jpg",
-            gateway:"046238f8-ec6c-4dde-a732-9230b7dc71a2"
+            name: "Pineapple Ginger",
+            price: "11",
+            desc: "",
+            image: "2.jpg",
+            gateway: "046238f8-ec6c-4dde-a732-9230b7dc71a2"
         },
         {
-            name:"Blue Pea",
-            price:"11",
-            desc:"",
-            image:"3.jpg",
-            gateway:"031403e1-ef5b-4310-aad2-ebfe85727cb7"
+            name: "Blue Pea",
+            price: "11",
+            desc: "",
+            image: "3.jpg",
+            gateway: "031403e1-ef5b-4310-aad2-ebfe85727cb7"
         },
         {
-            name:"Golden Latte",
-            price:"13",
-            desc:"",
-            image:"1.jpg",
-            gateway:"fe1334fc-2614-47c4-9237-a63610f8ff83"
+            name: "Golden Latte",
+            price: "13",
+            desc: "",
+            image: "1.jpg",
+            gateway: "fe1334fc-2614-47c4-9237-a63610f8ff83"
         },
-        
+
+    ],
+    VitaliScoops: [
+        {
+            name: "Turmeric Coffee",
+            price: "4",
+            desc: "",
+            image: "",
+            video: "video4.mp4",
+            gateway: "f6559b01-43f8-4254-bcc0-d6bb15a48c8c"
+        },
+        {
+            name: "Rose Sorbet",
+            price: "5",
+            desc: "",
+            image: "",
+            video: "video3.mp4",
+            gateway: "d5c28013-0e65-4443-b8d7-722b012369ae"
+        },
+        {
+            name: "Vanilla Olive Butter",
+            price: "4",
+            desc: "",
+            image: "",
+            video: "video1.mp4",
+            gateway: "1a1f5bf5-2047-4eea-b65f-959c0728ef24"
+        },
+        {
+            name: "Blue Pea Caramel",
+            price: "8",
+            desc: "",
+            image: "",
+            video: "video2.mp4",
+            gateway: "b2a2a9e4-807a-4148-b074-a0a440b833bd"
+        }
+    ],
+    ReviveJewel: [
+        {
+            name: "Bracelet",
+            price: "14",
+            desc: "",
+            image: "bracelets.jpeg",
+            gateway: "8ff60037-5182-46d2-83a4-8b46718c23f8"
+        },
+        {
+            name: "Ear Rings",
+            price: "16",
+            desc: "",
+            image: "earring.jpeg",
+            gateway: "8f5e45c6-3859-4fae-826b-9f7c568a810d"
+        },
+        {
+            name: "Necklace",
+            price: "20",
+            desc: "",
+            image: "Necklace.jpeg",
+            gateway: "f7bbf3a8-b326-408d-b903-3651c93667f5"
+        }
+    ],
+    Earthcrafts:[
+        {
+            name: "Glasses ( set of 4)",
+            price: "21",
+            desc: "",
+            image: "glasses.jpeg",
+            gateway: "ba681dd6-b897-42af-8d7c-967790fd140d" 
+        },
+        {
+            name: "Pot",
+            price: "25",
+            desc: "",
+            image: "pots.jpeg",
+            gateway: "8ba5c6eb-bbf1-4c61-be53-da800b6207f9" 
+        },
+        {
+            name: "Plates (set of 4)",
+            price: "32",
+            desc: "",
+            image: "plate.jpeg",
+            gateway: "1e773209-9f6a-49d4-b209-ba7d8d3f5132" 
+        },
+        {
+            name: "Pans (set of 2)",
+            price: "50",
+            desc: "",
+            image: "pans.jpeg",
+            gateway: "80af0f3a-fc68-4945-b163-601a0d847bc1" 
+        }
     ]
 }
